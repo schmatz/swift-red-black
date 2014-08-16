b59a5283ef150b5a3edd35c6a2f4d65ae84c7ed5
Michael Schmatz <michaelschmatz@gmail.com>
2014-08-15 23:44:58 -0700

    Wrote insertion and deletion tests
    Deletion tests are currently not passing

--- main.swift
+++ main.swift
@@ -8,10 +8,10 @@
 
 import Foundation
 
-let tree = RedBlackTree<Int,Int>()
+var tree = RedBlackTree<Int,Int>()
 
 func setup() {
-  let tree = RedBlackTree<Int,Int>()
+  tree = RedBlackTree<Int,Int>()
 }
 
 func testInsertion() {
@@ -45,8 +45,66 @@ func testInsertion() {
   assert(tree.root.left.right.red == true)
   assert(tree.root.right.left.red == true)
   assert(tree.root.right.right.red == true)
+  println("Insertion tests passed.")
+}
+
+func testDeletion() {
+  setup()
+  tree.insertKey(20)
+  tree.insertKey(18)
+  tree.insertKey(22)
+  tree.insertKey(16)
+  tree.insertKey(14)
+  tree.insertKey(24)
+  tree.insertKey(26)
+  //Delete the root and verify the state of the tree
+  tree.deleteKey(20)
+  assert(tree.root.key == 22)
+  assert(tree.root.left.key == 16)
+  assert(tree.root.left.left.key == 14)
+  assert(tree.root.left.right.key == 18)
+  assert(tree.root.right.key == 24)
+  assert(tree.root.right.right.key == 26)
+  assert(tree.root.right.left === tree.sentinel)
+  //Delete the root again
+  tree.deleteKey(22)
+  assert(tree.root.key == 24)
+  assert(tree.root.right.key == 26)
+  //And again
+  tree.deleteKey(24)
+  assert(tree.root.key == 16)
+  assert(tree.root.left.key == 14)
+  assert(tree.root.right.key == 26)
+  assert(tree.root.right.left.key == 18)
+  //and again
+  tree.deleteKey(16)
+  assert(tree.root.key == 18)
+  assert(tree.root.left.key == 14)
+  assert(tree.root.right.key == 26)
+  assert(tree.root.red == false)
+  assert(tree.root.left.red == false)
+  assert(tree.root.right.red == false)
+  //and again
+  tree.deleteKey(18)
+  assert(tree.root.key == 26)
+  assert(tree.root.left.key == 14)
+  assert(tree.root.red == false)
+  assert(tree.root.left.red == true)
+  //and again
+  tree.deleteKey(26)
+  assert(tree.root.key == 14)
+  assert(tree.root.red == false)
+  assert(tree.root.right === tree.sentinel)
+  assert(tree.root.left === tree.sentinel)
+  assert(tree.root.parent === tree.sentinel)
+  //and again
+  tree.deleteKey(14)
+  assert(tree.root === tree.sentinel)
+  //Try deleting a nonexistent node
+  tree.deleteKey(500)
+  println("Deletion tests passed.")
 }
 
 
 testInsertion()
-
+testDeletion()
