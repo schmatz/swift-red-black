fd11c5b3f5ab904025b6ff3aee0736afc07b7766
Michael Schmatz <michaelschmatz@gmail.com>
2014-08-15 22:59:34 -0700

    Added main file

---
+++ main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  redblack
+//
+//  Created by Michael Schmatz on 8/15/14.
+//  Copyright (c) 2014 MichaelSchmatz. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
+
